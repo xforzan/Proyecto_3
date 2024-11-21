42c213c41a5f9e41374ea2ae732852ff12c6ab1b
xforzan <nakhila2024@gmail.com>
2024-11-21 13:43:46 +0100

    .

---
+++ Proyecto_3/components/Header/header.js
@@ -0,0 +1,16 @@
+import './header.css'
+
+export const header = () =>
+    `<img src="/vite.svg" alt="logo">
+      <nav class="navigation">
+        <ul>
+          <li>
+            <button type="button" onclick="window.location.href=''">Inicio</button>
+          </li>
+          <li>
+            <button type="button" onclick="window.location.href=''">Explorar</button>
+          </li>
+        </ul>
+        <input type="text" name="Barra de búsqueda" id="searchBar" placeholder="Buscar">
+        <input type="submit" value="Buscar">
+      </nav>`
--- Proyecto_3/main.js
+++ Proyecto_3/main.js
@@ -1,1 +1,4 @@
 import './style.css'
+import { header } from './header.js' ;
+
+header()
